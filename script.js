ba67590f8f43b8670a1caa33f892d9bb9755e796
Dikontay <118036214+Dikontay@users.noreply.github.com>
2023-10-17 21:31:18 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Generate a random number between 1 and 10
+const randomNum = Math.floor(Math.random() * 10) + 1;
+
+// Get the HTML element where you want to display the random number
+const randomNumberElement = document.getElementById('task1');
+
+task2 = Math.floor(Math.random() * (799 - 89 + 1)) + 89;
+task3 = Math.floor(Math.random()*60)
+document.getElementById('task2').textContent+=`${task2}`
+document.getElementById('task3').textContent+=`${task3}`
+// Set the random number as the content of the HTML element
+randomNumberElement.textContent += ` ${randomNum}`;
+
+variants = [1, 0, -3]
+first_var = 0
+for(i = 0; i<length(variants); i++){
+    first_var = variants[0]
+    if (first_var == 0) {
+        
+    }
+}
+
+
